65eb9fd6ec4945786df2a55f72155c7d88d00444
Aryan Mann <mannarya@grinnell.edu>
2020-09-30 02:14:43 +0530

    Create amc-erp.js

---
+++ assets/js/amc-erp.js
@@ -0,0 +1,9 @@
+const insert_amc_erp = function() {
+  // load in the CSS
+  let v_header = document.getElementsByTagName("HEAD")[0];
+  let v_link = document.createElement("link");
+  v_link.type="text/css";
+  v_link.rel="stylesheet";
+  v_link.href="https://aryan-mann.github.io/bucket/assets/css/amc.css";
+  v_header.appendChild(t);
+}
